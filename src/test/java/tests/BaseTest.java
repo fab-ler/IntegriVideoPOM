5dc2f89157bc551efd3395b1c72da60f3c28063e
yuliyaklepova <yanju@tut.by>
2020-03-16 02:20:12 +0300

    basetest upd

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,13 @@
 package tests;
 
+import models.Card;
+import models.Project;
 import models.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.BillingPage;
 import pages.LoginPage;
 import pages.ProjectPage;
 import pages.SignUpPage;
@@ -15,7 +18,11 @@ public class BaseTest {
     ProjectPage projectPage;
     SignUpPage signUpPage;
     LoginPage loginPage;
+    BillingPage billingPage;
     User user;
+    Card card;
+    Project project;
+
     private WebDriver driver;
 
     @BeforeMethod
@@ -27,8 +34,10 @@ public class BaseTest {
         signUpPage = new SignUpPage(driver);
         loginPage = new LoginPage(driver);
         projectPage = new ProjectPage(driver);
+        billingPage = new BillingPage(driver);
         user = new User("hkj@mailinator.com", "0123456789");
-        ;
+        card = new Card(4242424242424242, 11, 2021, "Mike Brown");
+        project = new Project("test.io", "some extra data will be here");
     }
 
     @AfterMethod(alwaysRun = true)
