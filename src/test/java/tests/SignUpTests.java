1f6fcd118064a020d806741be690de0cff98cb99
yuliyaklepova <yanju@tut.by>
2020-03-16 02:17:55 +0300

    signuptest refactor

--- src/test/java/tests/SignUpTests.java
+++ src/test/java/tests/SignUpTests.java
@@ -6,13 +6,13 @@ import java.util.UUID;
 
 public class SignUpTests extends BaseTest {
 
-    @Test(invocationCount = 3)
+    @Test(invocationCount = 1)
     public void sighUp() {
         signUpPage.openPage();
         signUpPage.loginInput(UUID.randomUUID().toString() + "@gmail.com");
         signUpPage.passwordInput("0123456789");
         signUpPage.clickSubmit();
-        signUpPage.find();
-        //signUpPage.verifyMessageTextAlert("Message with instructions was sent");
+        signUpPage.findAlert();
+        //signUpPage.verifyMessageTextAlert();
     }
 }
